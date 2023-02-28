b510b9331e27c587b8b56a7227ba30fb2c8f5f8f
Linus Büning <deathsgun@protonmail.com>
2023-02-28 14:31:38 +0100

    Update handler.js

--- handler.js
+++ handler.js
@@ -1,1 +1,11 @@
-Test content
+module.exports = async (req, res) => {
+
+    // Access to the request body (only possible with the POST, PUT and PATCH methods)
+    // let body = req.json();
+    // if (!body || !body.name) {
+    //     res.status(400).set("Content-Type", "text/plain").send("Bad request");
+    //     return;
+    // }
+
+    res.status(200).set("Content-Type", "text/plain").send(`Hello World`);
+}
